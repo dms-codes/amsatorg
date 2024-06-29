183aa27b97174ce6af1447ff26cdb8eab56a2518
Donny Marthen Sitompul <donnymarthen.sitompul@gmail.com>
2024-06-29 11:04:34 +0200

    New

--- amsat.py
+++ amsat.py
@@ -2,6 +2,7 @@ import requests
 
 url_status = "https://www.amsat.org/status/api/v1/sat_info.php?name={}&hours={}"
 url_track = "https://www.amsat.org/track/api/v1/passes.php?location={}&object={}"
+url_satelitte_index = "https://www.amsat.org/amateur-satellite-index"
 
 sat_names = [
     "AISAT-1",
@@ -11,10 +12,12 @@ sat_names = [
     "FS-3",
 ]
 
-def GetStatus(satname):
-    res = requests.get(url=url_status.format(satname,"24"))
-    data = res.json()
-    res = "Status on Satellite {}\n{}\t\t{}\t\t{}\t{}\t\t{}\n".format(str.upper(satname),"Date", "Hour", "Callsign", "Report", "Grid")
+def get_json_from_requests(url,*params):
+    return requests.get(url=url.format(*params)).json()
+
+def get_status(sat_name):
+    data = get_json_from_requests(url_status,sat_name,"24")
+    res = "Status on Satellite {}\n{}\t\t{}\t\t{}\t{}\t\t{}\n".format(str.upper(sat_name),"Date", "Hour", "Callsign", "Report", "Grid")
     for d in data:
         date,hour = d['reported_time'].split("T")
         callsign = d['callsign']
@@ -23,9 +26,8 @@ def GetStatus(satname):
         res = res + "{}\t{}\t{}\t\t{}\t\t{}\n".format(date, hour, callsign, report, grid)
     return res
 
-def GetPasses(grid,satname):
-    res = requests.get(url=url_track.format(grid,satname))
-    data = res.json()
+def get_passes(grid,sat_name):
+    data = get_json_from_requests(url_track,grid,sat_name)
     res = "{}\t\t{}\t\t{}\t{}\t{}\t{}\t{}\n".format("Date", "Time", "Duration", "AOS Az.","Max Elev.","Max_Elev._Az", "LOS Az")
     for d in data:
         aos_date, aos_time = d['AOS_time'].split("T")
@@ -37,30 +39,30 @@ def GetPasses(grid,satname):
         res = res + "{}\t{}\t{}\t\t{}\t{}\t\t{}\t\t{}\n".format(aos_date,aos_time,pass_duration, aos_azimuth,max_elevation,max_elev_azimuth,los_azimuth)
     return res
 
-def Get1stPass(grid,satname):
-    res = requests.get(url=url_track.format(grid,satname))
-    data = res.json()
-    res = data
-#    aos_date, aos_time = d['AOS_time'].split("T")
-#    pass_duration = d['pass_duration'][-6:]
-#    aos_azimuth = d['AOS_azimuth']
-#    max_elevation = d['max_elevation']
-#    max_elev_azimuth = d['max_elev_azimuth']
-#    los_azimuth= d['LOS_azimuth']
-#    res = aos_date, aos_time,pass_duration,aos_azimuth,max_elevation,max_elev_azimuth,los_azimuth
-#    print(res)
+def get_1st_pass(grid,sat_name):
+    data = get_json_from_requests(url_track,grid,sat_name)
+    res = "{}\t\t{}\t\t{}\t{}\t{}\t{}\t{}\n".format("Date", "Time", "Duration", "AOS Az.","Max Elev.","Max_Elev._Az", "LOS Az")
+    for d in data:
+        aos_date, aos_time = d['AOS_time'].split("T")
+        pass_duration = d['pass_duration'][-6:]
+        aos_azimuth = d['AOS_azimuth']
+        max_elevation = d['max_elevation']
+        max_elev_azimuth = d['max_elev_azimuth']
+        los_azimuth= d['LOS_azimuth']
+        res = res + "{}\t{}\t{}\t\t{}\t{}\t\t{}\t\t{}\n".format(aos_date,aos_time,pass_duration, aos_azimuth,max_elevation,max_elev_azimuth,los_azimuth)
+        print(res)
+        break
     return res
 
-def GetAllSat1stPass(grid,sat_names):
+def get_1st_pass_all_sats(grid,sat_names):
     FirstPasses = []
     for sat_name in sat_names:
-        First_Pass = Get1stPass(grid,sat_name)
-        print(sat_name, First_Pass)
-
+        First_Pass = get_1st_pass(grid,sat_name)
 
 if __name__ == "__main__":
-    grid = "OI33"
-    satname = "AO-91"
-    print(GetStatus(satname))
-#    print(GetPasses(grid,satname))
-#    GetAllSat1stPass(grid,sat_names)
+    grid = "JO28UX"
+    sat_name = "AO-91"
+    sat_names = [sat_name]
+    #print(get_status(sat_name))
+    #print(get_passes(grid,sat_name))
+    get_1st_pass_all_sats(grid,sat_names)
